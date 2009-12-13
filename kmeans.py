5f0ffc65a5d30688c111d70a33aec9ab825e960e
Rafael Fonseca <r4f4rfs@gmail.com>
2009-12-13 00:38:38 -0200

    Add some default functions.
    - choose_initial: function to return k random centroids.
    - choose_initial_pp: function to return k random centroids but using
    probabilites
    - calc_centroid: function to calculate a centroid from a cluster.

--- kmeans.py
+++ kmeans.py
@@ -31,3 +31,56 @@ class Kmeans(object):
 
     def result(self):
         return self.centroids
+
+
+def choose_initial(data, k, distfunc=None):
+    """
+    Choose randomly k different centroids.
+
+    :data The elements being clustered
+    :k The number of clusters
+    :distfunc Ignored
+    """
+    return random.sample(data, k)
+
+def choose_initial_pp(data, k, distfunc):
+    """
+    Choose randomly k different centroids using the kmeans++ heuristic
+    by David Arthur and Sergei Vassilvitskii (see the article "k-means++:
+    The Advantages of Careful Seeding".
+
+    :data The elements being clustered
+    :k The number of clusters
+    :distfunc Function to calculate the distance between two elements.
+    """
+
+    # Calculate squared distance
+    distance2 = lambda(c, x): calcdist.setdefault((c, x),
+            calcdist.setdefault((x, c), distfunc(x, c)))
+
+    # The first centroid is a random one
+    centroids = [random.choice(data)]
+    # Table to store the calculated values, avoiding duplicated calculations
+    calcdists = dict()
+
+    while len(centroids) < k:
+        mindists = [min((distance2(c, x), x) for c in centroids)
+                for x in data if x is not c]
+        # Divide because we add it twice: first for (c, x) and then for (x, c)
+        totaldist = float(sum(mindists) / 2)
+        for d, x in mindists:
+            if x not in centroids and random.random() < d / totaldist:
+                centroids.append(x)
+                break
+
+    return centroids
+
+def calc_centroid(cluster):
+    """
+    Calculate the centroid from the given cluster.
+
+    Note: This function assumes that the methods __add__ and __div__ are
+    correctly set on the cluster's elements. Otherwise, use your own function.
+    """
+
+    return sum(cluster) / len(cluster)
