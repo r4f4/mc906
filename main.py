16728097f30280ac6c85a2de743d1d2337dd6f26
Rafael Fonseca <r4f4rfs@gmail.com>
2009-12-14 04:01:22 -0200

    Just parse files if needed.
    - Parsing is now stored.

--- main.py
+++ main.py
@@ -2,10 +2,18 @@
 
 import os
 import numpy
+import pickle
 from kmeans import Kmeans, choose_initial, choose_initial_pp
 from parse import Parser
 from re import split
 from util import distance, calc_centroid, normalize, get_clusters
+from gc import collect
+
+# Path to the directory containing the messages
+path = './cluster-txt/messages/'
+fname_stemmed = 'stemparsed.txt'
+fname = 'parsed.txt'
+fstopname = 'english'
 
 def print_result(freqs, errors):
     for i, (f, e) in enumerate(zip(freqs, errors)):
@@ -31,22 +39,49 @@ def calc_error(freqs):
         errors.append(err)
     return errors
 
-if __name__ == "__main__":
-    parser = Parser('english')
-    # Path to the directory containing the messages
-    path="./cluster-txt/messages/"
+def parsefiles(filelist):
+    print 'Parsing files using stemming...',
+    parser = Parser(fstopname)
+    parser.parse(filelist, True)
+    with open(fname_stemmed, 'w') as f:
+        pickle.dump(parser, f)
+    del parser
+    collect()
+    print 'done'
+    print 'Parsing file whithout stemming...',
+    parser = Parser(fstopname)
+    parser.parse(filelist, False)
+    with open(fname, 'w') as f:
+        pickle.dump(parser, f)
+    del parser
+    collect()
+    print 'done'
 
+
+if __name__ == "__main__":
     filelist = [(path + f) for f in os.listdir(path)]
 
-    for i in True, False:
-        for j in True, False:
-            parser.parse(filelist, i)
-            map(lambda doc: normalize(doc, parser.words, j), parser.docset)
+    print 'Files need parsing:',
+    if not (os.path.exists(fname_stemmed) or os.path.exists(fname)):
+        print 'yes'
+        parsefiles(filelist)
+    else:
+        print 'no'
+
+    for stem in True, False:
+        if stem is True:
+            with open(fname_stemmed, 'r') as f:
+                parser = pickle.load(f)
+        else:
+            with open(fname, 'r') as f:
+                parser = pickle.load(f)
+        for idf in True, False:
+            map(lambda doc: normalize(doc, parser.words, idf), parser.docset)
             for chooser in choose_initial_pp, choose_initial:
                 for k in 10, 20, 30, 40:
                     errors = []
-                    print '\nStemming words: %s' % i
-                    print 'Using IDF: %s' % j
+                    print '\nStemming words: %s' % stem
+                    print 'Using IDF: %s' % idf
                     print 'Running with %d centroids' % k
                     if chooser is choose_initial:
                         print 'Chooser: normal'
@@ -60,3 +95,5 @@ if __name__ == "__main__":
                         errors.append(sum(calc_error(freqs)))
                     print 'Error mean: %d and median: %d' % \
                         (numpy.mean(errors), numpy.median(errors))
+        del parser
+        collect()
