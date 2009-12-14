3f3f7df700055f214740d9e2bc2da1b0285ec7e6
Rafael Fonseca <r4f4rfs@gmail.com>
2009-12-13 22:38:42 -0200

    Refactor Kmeans class.

--- kmeans.py
+++ kmeans.py
@@ -1,52 +1,5 @@
 import random
 
-class Kmeans(object):
-    """
-    k-means algorithm
-    """
-
-    def __init__(self, data, k, distfunc, centroidfunc=calc_centroid,
-            chooser=choose_initial, tol=0.0005):
-        """
-        The K-means algorithm
-
-        :data A data set
-        :k The number of clusters
-        :distfunc A function to calculate the distance between two elements
-        from data.
-        :centroidfunc A function to calculate the centroid of the cluster. It
-        will receive a cluster.
-        :chooser A function to choose the initial k centroids. It must receive
-        the data set, the number of clusters k and the distfunc.
-        :tol Error tolerance
-        """
-
-        self.centroids = chooser(data, k, distfunc)
-
-        err = -1
-        while True:
-            bins = [set() for _ in xrange(k)]
-
-            for i in data:
-                ml = [(distfunc(c, i), ic) for ic, c in enumerate(self.centroids)]
-                ml_min, _ =  min(ml)
-                c = random.choice([j for m, j in ml if m == ml_min])
-                bins[c].add(i)
-
-            for bi, b in enumerate(bins):
-                self.centroids[bi] = centroidfunc(b)
-
-            olderr = err
-            err = sum([sum([distfunc(d, self.centroids[c]) for d in b])
-                      for c, b in enumerate(bins)])
-
-            if err - olderr < tol:
-                break
-
-    def result(self):
-        return self.centroids
-
-
 def choose_initial(data, k, distfunc=None):
     """
     Choose randomly k different centroids.
@@ -69,13 +22,10 @@ def choose_initial_pp(data, k, distfunc):
     """
 
     # Calculate squared distance
-    distance2 = lambda(c, x): calcdist.setdefault((c, x),
-            calcdist.setdefault((x, c), distfunc(x, c)))
+    distance2 = lambda(c, x): distance(c, x)**2
 
     # The first centroid is a random one
     centroids = [random.choice(data)]
-    # Table to store the calculated values, avoiding duplicated calculations
-    calcdists = dict()
 
     while len(centroids) < k:
         mindists = [min((distance2(c, x), x) for c in centroids)
@@ -89,12 +39,55 @@ def choose_initial_pp(data, k, distfunc):
 
     return centroids
 
-def calc_centroid(cluster):
+class Kmeans(object):
     """
-    Calculate the centroid from the given cluster.
-
-    Note: This function assumes that the methods __add__ and __div__ are
-    correctly set on the cluster's elements. Otherwise, use your own function.
+    k-means algorithm
     """
 
-    return sum(cluster) / len(cluster)
+    def __init__(self, data, k, distfunc=None, centroidfunc=None,
+            chooser=choose_initial, tol=0.0005):
+        """
+        The K-means algorithm
+
+        :data A data set
+        :k The number of clusters
+        :distfunc A function to calculate the distance between two elements
+        from data.
+        :centroidfunc A function to calculate the centroid of the cluster. It
+        will receive a cluster.
+        :chooser A function to choose the initial k centroids. It must receive
+        the data set, the number of clusters k and the distfunc.
+        :tol Error tolerance
+        """
+
+        assert distfunc is not None, "distfunc can't be None"
+        assert centroidfunc is not None, "centroidfunc can't be None"
+
+        self.centroids = chooser(data, k, distfunc)
+
+        print 'There are %d initial centroids' % len(self.centroids)
+        err = -1
+        niter = 1
+        while True:
+            bins = [set() for _ in xrange(k)]
+            print 'Iteration #%d' % niter
+            niter += 1
+
+            for i in data:
+                ml = [(distfunc(c, i), ic) for ic, c in enumerate(self.centroids)]
+                ml_min, _ =  min(ml)
+                c = random.choice([j for m, j in ml if m == ml_min])
+                bins[c].add(i)
+
+            for bi, b in enumerate(bins):
+                self.centroids[bi] = centroidfunc(b)
+
+            olderr = err
+            err = sum([sum([distfunc(d, self.centroids[c]) for d in b])
+                      for c, b in enumerate(bins)])
+
+            if err - olderr < tol:
+                break
+
+    def result(self):
+        return self.centroids
